2b43dfbb88b5a99e5dba6a8a939717a7b0030ea2
kirtan <kirtanthakkar30@gmail.com>
2025-07-04 23:25:27 +0530

    removed the unused request

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,36 +1,36 @@
-import { connectDB } from "@/lib/db";
-import { NextResponse } from "next/server";
-import { User } from "@/models/user";
-export async function GET(request: Request) {
-  await connectDB();
-  return NextResponse.json({
-    message: "connected to the datbase succesfully",
-  });
-}
-export async function POST(request: Request) {
-  await connectDB();
-  const body = await request.json();
-  const { name, email, password } = body;
-  if (!name || !email || !password) {
-    return NextResponse.json(
-      {
-        message: "Please fill all the fields",
-      },
-      { status: 400 }
-    );
-  } else {
-    const user = await new User({
-      name: name,
-      email: email,
-      password: password,
-      credentials: "crendentials",
-    });
-    await user.save();
-    return NextResponse.json(
-      {
-        message: "the user has been created succesfully",
-      },
-      { status: 201 }
-    );
-  }
-}
+// import { connectDB } from "@/lib/db";
+// import { NextResponse } from "next/server";
+// import { User } from "@/models/user";
+// export async function GET(request: Request) {
+//   await connectDB();
+//   return NextResponse.json({
+//     message: "connected to the datbase succesfully",
+//   });
+// }
+// export async function POST(request: Request) {
+//   await connectDB();
+//   const body = await request.json();
+//   const { name, email, password } = body;
+//   if (!name || !email || !password) {
+//     return NextResponse.json(
+//       {
+//         message: "Please fill all the fields",
+//       },
+//       { status: 400 }
+//     );
+//   } else {
+//     const user = await new User({
+//       name: name,
+//       email: email,
+//       password: password,
+//       credentials: "crendentials",
+//     });
+//     await user.save();
+//     return NextResponse.json(
+//       {
+//         message: "the user has been created succesfully",
+//       },
+//       { status: 201 }
+//     );
+//   }
+// }
