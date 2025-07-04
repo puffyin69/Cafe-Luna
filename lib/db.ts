794c82c1f16ca5f46cbc4b1f0c8de3f3703c2304
kirtan <kirtanthakkar30@gmail.com>
2025-07-04 23:51:12 +0530

    removed the unsed request

--- lib/db.ts
+++ lib/db.ts
@@ -1,27 +1,27 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI as string;
+// const MONGO_URI = process.env.MONGO_URI as string;
 
-if (!MONGO_URI) {
-  throw new Error("Please define the MMONGO_URI environment variable");
-}
+// if (!MONGO_URI) {
+//   throw new Error("Please define the MMONGO_URI environment variable");
+// }
 
-export const connectDB = async () => {
-  try {
-    if (mongoose.connection.readyState >= 1) {
-      // Already connected
-      return mongoose.connection.asPromise();
-    }
-    await mongoose.connect(MONGO_URI);
-    console.log("MongoDB connected successfully");
-    return mongoose.connection;
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error("An error occurred:", err.message);
-      throw err;
-    } else {
-      console.error("An error occurred:", err);
-      throw err;
-    }
-  }
-};
+// export const connectDB = async () => {
+//   try {
+//     if (mongoose.connection.readyState >= 1) {
+//       // Already connected
+//       return mongoose.connection.asPromise();
+//     }
+//     await mongoose.connect(MONGO_URI);
+//     console.log("MongoDB connected successfully");
+//     return mongoose.connection;
+//   } catch (err) {
+//     if (err instanceof Error) {
+//       console.error("An error occurred:", err.message);
+//       throw err;
+//     } else {
+//       console.error("An error occurred:", err);
+//       throw err;
+//     }
+//   }
+// };
